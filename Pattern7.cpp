dcf5c5563f811352a9cb63e29c6ae3abaa798ffb
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2026-09-17 11:01:57 +0530

    Add files via upload

---
+++ Pattern7.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+void Pattern(int n){
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-i-1;j++){
+        cout<<" ";
+    }
+    for(int j=0;j<2*i+1;j++){
+        cout<<"*";
+    }
+    for(int j=0;j<n-i-1;j++){
+        cout<<" ";
+    }
+    cout<<endl;
+}
+}
+void PatternReverse(int n){
+for(int i=0;i<n;i++){
+
+   for(int j=0;j<i;j++){
+        cout<<" ";
+    }
+    for(int j=0;j<2*n-(2*i+1);j++){
+        cout<<"*";
+    }
+    for(int j=0;j<i;j++){
+        cout<<" ";
+    }
+    cout<<endl;
+}
+}
+int main(){
+    int t;
+    cout<<"Enter No. Of Test Cases";
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+        Pattern(n);
+         PatternReverse(n);
+    }
+return 0;
+}
