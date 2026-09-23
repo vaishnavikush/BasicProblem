d3879947696f798a1ccb4efcf5985b7a70e28518
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2026-09-23 10:31:42 +0530

    Add files via upload

---
+++ Pattern15.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+void Pattern(int n){
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-i-1;j++){
+        cout<<" ";
+    }
+    char ch='A';
+    int breakPoint=(2*i+1)/2;
+    for(int j=0;j<2*i+1;j++){
+        cout<<ch;
+        if(j<breakPoint){
+            ch++;
+        }
+        else{
+            ch--;
+        }
+    }
+    for(int j=0;j<n-i-1;j++){
+        cout<<" ";
+    }
+    cout<<endl;
+}
+}
+int main(){
+int t;
+cout<<"Enter No. Of Cases";
+cin>>t;
+for(int i=0;i<t;i++){
+    int n;
+    cin>>n;
+    Pattern(n);
+}
+return 0;
+}
