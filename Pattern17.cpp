4c7f2b01961b1e153840f0584307f3e3d0fcf4e6
Vaishnavi kushwaha <vaishnavikushwaha53@gmail.com>
2026-09-24 10:19:02 +0530

    Add files via upload

---
+++ Pattern17.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+void Pattern(int n){
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-i;j++){
+        cout<<"*";
+    }
+    for(int j=0;j<2*i;j++){
+        cout<<" ";
+    }
+    for(int j=0;j<n-i;j++){
+        cout<<"*";
+    }
+    if(i==n-1){
+        break;
+    }
+    else{
+    cout<<endl;
+    }
+}
+}
+void Pattern2(int n){
+for(int i=n;i>=0;i--){
+    for(int j=0;j<n-i;j++){
+        cout<<"*";
+    }
+    for(int j=0;j<2*i;j++){
+        cout<<" ";
+    }
+    for(int j=0;j<n-i;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+}
+int main(){
+int t;
+cout<<"Enter No. Of Test Cases";
+cin>>t;
+for(int i=0;i<t;i++){
+    int n;
+    cin>>n;
+    Pattern(n);
+    Pattern2(n);
+}
+return 0;
+}
